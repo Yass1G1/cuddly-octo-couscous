2598339c2da91341af2fd271f8c9920536ff2dc5
Yass <67269173+Yass1G1@users.noreply.github.com>
2022-08-07 22:37:10 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+Interactions avec le site : 
+	Partie haute du site : 
+		Photo : lors du click => Redirection vers la version vectoriel de la photo (grand format)
+		Adresse : lors du click => Google Maps
+		Email : lors du click => App de messagerie	
+	Corps du site : 
+		Diplômes : 
+			Passage de la souris sur la certif en anglais : animation underline
+		Chaque section : 
+			Passage de la souris sur la section : affichage ponctuel 
+			Lors du click simple : Affichage "permanent"
+			Lors du double click : Désaffichage du contenu
+		Compétences : 
+			Certif anglais :
+				Lors du click => Redirection vers le fichier en PDF
---
+++ script INUTILE.js
@@ -0,0 +1,27 @@
+function droptest() {
+    document.getElementById("drop-1").style.display = "block"
+};
+function dropcan() {
+    document.getElementById("drop-1").style.display = "none"
+};
+
+function droptest() {
+    document.getElementById("drop-2").style.display = "block"
+};
+function dropcan() {
+    document.getElementById("drop-2").style.display = "none"
+};
+
+function droptest() {
+    document.getElementById("drop-3").style.display = "block"
+};
+function dropcan() {
+    document.getElementById("drop-3").style.display = "none"
+};
+
+function droptest() {
+    document.getElementById("drop-4").style.display = "block"
+};
+function dropcan() {
+    document.getElementById("drop-4").style.display = "none"
+};
